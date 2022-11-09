f2cb81dde2afd54a1627fdd1603f9dd73ad3fe3a
Alan Lin <shijun.lin@leadvisor.com>
2022-11-08 22:22:53 -0500

    initialized spring project

---
+++ backend/src/test/java/com/groupnameundefined/utstudentapplication/UtStudentapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupnameundefined.utstudentapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtStudentapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
