538f35d65054b5f3463603bf527f3a2d6b0d7aea
Alan Lin <shijun.lin@leadvisor.com>
2022-11-19 05:43:18 -0500

    Setup frontend structure with samples

---
+++ frontend/temp/PassWordDialog.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Arrays;
+
+public class PassWordDialog extends JDialog {
+
+    private final JLabel jlblUsername = new JLabel("Username");
+    private final JLabel jlblPassword = new JLabel("Password");
+
+    private final JTextField jtfUsername = new JTextField(15);
+    private final JPasswordField jpfPassword = new JPasswordField();
+
+    private final JButton jbtOk = new JButton("Login");
+    private final JButton jbtCancel = new JButton("Cancel");
+
+    private final JLabel jlblStatus = new JLabel(" ");
+
+    public PassWordDialog() {
+        this(null, true);
+    }
+
+    public PassWordDialog(final JFrame parent, boolean modal) {
+        super(parent, modal);
+
+        JPanel p3 = new JPanel(new GridLayout(2, 1));
+        p3.add(jlblUsername);
+        p3.add(jlblPassword);
+
+        JPanel p4 = new JPanel(new GridLayout(2, 1));
+        p4.add(jtfUsername);
+        p4.add(jpfPassword);
+
+        JPanel p1 = new JPanel();
+        p1.add(p3);
+        p1.add(p4);
+
+        JPanel p2 = new JPanel();
+        p2.add(jbtOk);
+        p2.add(jbtCancel);
+
+        JPanel p5 = new JPanel(new BorderLayout());
+        p5.add(p2, BorderLayout.CENTER);
+        p5.add(jlblStatus, BorderLayout.NORTH);
+        jlblStatus.setForeground(Color.RED);
+        jlblStatus.setHorizontalAlignment(SwingConstants.CENTER);
+
+        setLayout(new BorderLayout());
+        add(p1, BorderLayout.CENTER);
+        add(p5, BorderLayout.SOUTH);
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        jbtOk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Make API request here
+                if (Arrays.equals("stackoverflow".toCharArray(), jpfPassword.getPassword())
+                        && "stackoverflow".equals(jtfUsername.getText())) {
+                    parent.setVisible(true);
+                    setVisible(false);
+                } else {
+                    jlblStatus.setText("Invalid username or password");
+                }
+            }
+        });
+        jbtCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                parent.dispose();
+                System.exit(0);
+            }
+        });
+    }
+}
