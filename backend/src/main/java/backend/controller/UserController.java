d3d2662d7f448498987bed5b5d01498db10bf0f7
林世骏 <shijun.lin@leadvisor.com>
2022-11-21 19:36:06 -0500

    Refactored according to CLEAN

--- backend/src/main/java/backend/controller/UserController.java
+++ backend/src/main/java/backend/controller/UserController.java
@@ -1,75 +1,18 @@
 package backend.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import backend.model.User;
-import backend.repository.UserRepository;
 
-@CrossOrigin
-@RestController
-@RequestMapping("/api")
-public class UserController {
+public interface UserController {
 
-    @Autowired
-    UserRepository userRepository;
+    ResponseEntity<String> getUserGreeting();
 
-    @GetMapping("/users/greeting")
-    public ResponseEntity<String> getUserGreeting() {
-        return new ResponseEntity<>("User: Greetings!", HttpStatus.OK);
-    }
+    ResponseEntity<List<User>> getAllUsers();
 
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> getAllUsers() {
-        try {
-            List<User> users = new ArrayList<User>();
+    ResponseEntity<User> getUserById(Long id);
 
-            userRepository.findAll().forEach(users::add);
-
-            if (users.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity<>(users, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @GetMapping("/users/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
-        Optional<User> userData = userRepository.findById(id);
-
-        if (userData.isPresent()) {
-            return new ResponseEntity<>(userData.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        try {
-            User _user = userRepository.save(new User(user.getName()));
-
-            return new ResponseEntity<>(_user, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+    ResponseEntity<User> createUser(User user);
 }
---
+++ backend/src/main/java/backend/service/imp/UserServiceImp.java
@@ -0,0 +1,41 @@
+package backend.service.imp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import backend.model.User;
+import backend.repository.UserRepository;
+import backend.service.UserService;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<User>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user;
+    }
+
+    @Override
+    public User createUser(User user) {
+        User _user = userRepository.save(new User(user.getName()));
+
+        return _user;
+    }
+}
