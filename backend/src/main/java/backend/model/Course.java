99f14f5b6c5406c77869b47d3b39b322a30cc42d
Alan Lin <shijun.lin@leadvisor.com>
2022-11-25 00:18:41 -0500

    Added block models

---
+++ backend/src/main/java/backend/model/Course.java
@@ -0,0 +1,88 @@
+package backend.model;
+
+import java.util.ArrayList;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "section_code")
+    private String sectionCode;
+
+    @Column(name = "campus")
+    private String campus;
+
+    @OneToMany(cascade = { CascadeType.REMOVE }, orphanRemoval = true)
+    private ArrayList<Section> sections;
+
+    public Course(String name, String code, String sectionCode, String campus, ArrayList<Section> sections) {
+        this.name = name;
+        this.code = code;
+        this.sectionCode = sectionCode;
+        this.campus = campus;
+        this.sections = sections;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getSectionCode() {
+        return sectionCode;
+    }
+
+    public void setSectionCode(String sectionCode) {
+        this.sectionCode = sectionCode;
+    }
+
+    public String getCampus() {
+        return campus;
+    }
+
+    public void setCampus(String campus) {
+        this.campus = campus;
+    }
+
+    public ArrayList<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(ArrayList<Section> sections) {
+        this.sections = sections;
+    }
+
+}
---
+++ backend/src/main/java/backend/model/SectionBlock.java
@@ -0,0 +1,42 @@
+package backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "section_block")
+public class SectionBlock extends Block {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id", referencedColumnName = "id")
+    private Section section;
+
+    public SectionBlock() {
+    }
+
+    public SectionBlock(Section section) {
+        this.section = section;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
+}
