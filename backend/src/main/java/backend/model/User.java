e3054d64c87f837393d8830dceae6d1a0f18ad5b
Alan Lin <shijun.lin@leadvisor.com>
2022-11-24 20:55:42 -0500

    Associated user with studentprofile

--- backend/src/main/java/backend/model/User.java
+++ backend/src/main/java/backend/model/User.java
@@ -1,10 +1,13 @@
 package backend.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,6 +19,9 @@ public class User {
 
     @Column(name = "name")
     private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+    private StudentProfile studentProfile;
 
     public User() {
     }
