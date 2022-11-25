d6b1b1b51ed79cbf7497e8fb58efe2a002d323b0
Alan Lin <shijun.lin@leadvisor.com>
2022-11-24 20:48:13 -0500

    Initialzied SocialMediaProfile model with studentprofile rel

--- backend/src/main/java/backend/model/SocialMediaProfile.java
+++ backend/src/main/java/backend/model/SocialMediaProfile.java
@@ -6,7 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import backend.model.StudentProfile;
 
 @Entity
 @Table(name = "social_media_profile")
@@ -15,8 +18,10 @@ public class SocialMediaProfile {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    // @JoinColumn()
-
+    @OneToOne
+    @JoinColumn(name = "studentprofile_id", referencedColumnName = "id")
+    private StudentProfile studentProfile;
+    
     public SocialMediaProfile() {
     }
 }
