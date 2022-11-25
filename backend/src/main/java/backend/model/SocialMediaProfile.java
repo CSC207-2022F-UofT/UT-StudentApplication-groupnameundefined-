88979807e765cd93ec01142e2c15c53be763c4fe
Alan Lin <shijun.lin@leadvisor.com>
2022-11-24 20:37:38 -0500

    Initialized socialmediaprofile files

---
+++ backend/src/main/java/backend/model/SocialMediaProfile.java
@@ -0,0 +1,22 @@
+package backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "social_media_profile")
+public class SocialMediaProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    // @JoinColumn()
+
+    public SocialMediaProfile() {
+    }
+}
