541435a219a7d2875960ecf244d10fc3b50808c8
Jimmy Hao <hzc011213@gmail.com>
2022-11-24 19:10:20 -0500

    Request v1

--- backend/src/main/java/backend/model/Request.java
+++ backend/src/main/java/backend/model/Request.java
@@ -1,21 +1,28 @@
 package backend.model;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.Date;
 
 public class Request {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     // status in {PENDING, APPROVED, DENIED}
-    String status;
+    @Column(name = "status")
+    private String status;
     @OneToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
-    User from;
+    @JoinColumn(name = "from_id", referencedColumnName = "id")
+    private User from;
     @OneToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
-    User to;
-    String message;
-    Timestamp time;
+    @JoinColumn(name = "to_id", referencedColumnName = "id")
+    private User to;
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "time")
+    private Timestamp time;
     final String defaultMessage = "Hi " + to.getName() + ", this is " + from.getName() + ", nice to meet you!";
 
     public Request(User from, User to, String message) {
@@ -78,5 +85,25 @@ public class Request {
 
     public void setTime(Timestamp time) {
         this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Request " + this.id + ": from " + this.from.getName()
+                + "to " + this.to.getName() + " is " + this.status;
+        return s;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+
+        Request request = (Request) o;
+        return this.id.equals(request.id);
     }
 }
