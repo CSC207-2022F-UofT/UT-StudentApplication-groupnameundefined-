d5030176e6533c51642eeb89c42c469570c08eed
Jimmy Hao <hzc011213@gmail.com>
2022-11-25 00:17:03 -0500

    StudentProfile change

--- backend/src/main/java/backend/model/StudentProfile.java
+++ backend/src/main/java/backend/model/StudentProfile.java
@@ -1,13 +1,7 @@
 package backend.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "student_profile")
@@ -26,7 +20,49 @@ public class StudentProfile {
     @Column(name = "college")
     private String college;
 
-    @Column(name = "")
+    @Column(name = "enrolment_year")
+    private Integer enrolmentYear;
+
+    @ElementCollection
+    @Column(name = "courses")
+    private ArrayList<String> courses;
+
+//    @Column(name = "habits")
+//    private Habits habits;
+
+    // this variable is not contained in "student_profile" table
+    @ManyToMany
+    @JoinTable(
+            name = "timetable",
+            joinColumns = @JoinColumn(name = "studentprofile_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "block_id", referencedColumnName = "id")
+    )
+    private ArrayList<Block> timetable;
+
+    @OneToOne
+    @JoinColumn(name = "socialmediaprofile_id", referencedColumnName = "id")
+    private SocialMediaProfile socialMediaProfile;
+
+    public StudentProfile() {
+    }
+
+    public StudentProfile(User user) {
+        this.user = user;
+        this.program = program;
+        this.college = college;
+        this.enrolmentYear = enrolmentYear;
+    }
+
+    public StudentProfile(User user, String program, String college, Integer enrolmentYear) {
+        this.user = user;
+        this.program = program;
+        this.college = college;
+        this.enrolmentYear = enrolmentYear;
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public User getUser() {
         return user;
@@ -36,4 +72,51 @@ public class StudentProfile {
         this.user = user;
     }
 
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public Integer getEnrolmentYear() {
+        return enrolmentYear;
+    }
+
+    public void setEnrolmentYear(Integer enrolmentYear) {
+        this.enrolmentYear = enrolmentYear;
+    }
+
+    public ArrayList<String> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(ArrayList<String> courses) {
+        this.courses = courses;
+    }
+
+    public ArrayList<Block> getTimetable() {
+        return timetable;
+    }
+
+    public void setTimetable(ArrayList<Block> timetable) {
+        this.timetable = timetable;
+    }
+
+    public SocialMediaProfile getSocialMediaProfile() {
+        return socialMediaProfile;
+    }
+
+    public void setSocialMediaProfile(SocialMediaProfile socialMediaProfile) {
+        this.socialMediaProfile = socialMediaProfile;
+    }
 }
