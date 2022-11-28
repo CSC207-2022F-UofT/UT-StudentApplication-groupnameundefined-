30c0107c52a356a061f964b4daeaa8eb8b1502aa
hzelg <bguoac@connect.ust.hk>
2022-11-28 17:49:52 -0500

    Fix .gitignore and typos

--- backend/src/test/java/backend/habit/HabitControllerTests.java
+++ backend/src/test/java/backend/habit/HabitControllerTests.java
@@ -7,7 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import backend.controller.UserController;
+import backend.controller.HabitController;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +15,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import backend.controller.HabitController;
 
 public class HabitControllerTests {
 
@@ -32,7 +31,7 @@ public class HabitControllerTests {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/api/habit/notification")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/api/habits/notification")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Habits got.")));
     }
 
