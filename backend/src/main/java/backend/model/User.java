b19af93b1280db0920d032b572233ca69846508b
Alan Lin <shijun.lin@leadvisor.com>
2022-11-29 05:04:12 -0500

    Modified according to other entities and added docs

--- backend/src/main/java/backend/model/User.java
+++ backend/src/main/java/backend/model/User.java
@@ -1,13 +1,23 @@
 package backend.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
+
+    @Setter(AccessLevel.NONE)
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -24,24 +34,24 @@ public class User {
     @Column(name = "phone")
     private String phone;
 
-    @Column(name = "loginStatus")
+    @Column(name = "login_status")
     private boolean loginStatus;
 
-    @Column(name = "joinedTime")
+    @Column(name = "joined_time")
     private Timestamp joinedTime;
 
-    @Column(name = "lastActiveTime")
+    @Column(name = "last_active_time")
     private Timestamp lastActiveTime;
 
     @Column(name = "friends")
     @ManyToMany
-    private List<User> friends;
+    private Set<User> friends;
 
-    @Column(name = "blackList")
+    @Column(name = "black_list")
     @ManyToMany
-    private List<User> blackList;
+    private Set<User> blackList;
 
-    @OneToOne(cascade = { CascadeType.ALL }, mappedBy = "user")
+    @OneToOne(cascade = {CascadeType.ALL}, mappedBy = "user")
     private StudentProfile studentProfile;
 
 //    @OneToMany(cascade = { CascadeType.ALL }, mappedBy = "from")
@@ -52,7 +62,6 @@ public class User {
 
 
     public User() {
-
     }
 
     public User(String name, String email, String password, String phone) {
@@ -63,109 +72,67 @@ public class User {
         this.loginStatus = false;
         this.joinedTime = new Timestamp(System.currentTimeMillis());
         this.lastActiveTime = new Timestamp(System.currentTimeMillis());
-        this.friends = new ArrayList<User>();
-        this.blackList = new ArrayList<User>();
-    }
-
-    public Long getId() { return this.id; }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public String getPhone() {
-        return this.phone;
-    }
-
-    public boolean getLoginStatus() { return this.loginStatus; }
-
-    public Timestamp getJoinedTime() {
-        return this.joinedTime;
-    }
-
-    public Timestamp getLastActiveTime() {
-        return this.lastActiveTime;
-    }
-
-    public List<User> getFriends(){
-        return this.friends;
-    }
-
-    public List<User> getBlackList(){
-        return this.blackList;
-    }
-
-    public StudentProfile getStudentProfile() {
-        return studentProfile;
     }
 
     public void setStudentProfile(StudentProfile studentProfile) {
         this.studentProfile = studentProfile;
+        studentProfile.setUser(this);
     }
 
-
-    public void setName(String name) {
-        this.name = name;
+    public void initializeBlacklist() {
+        this.blackList = new HashSet<>();
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    /**
+     * Checks whether the target user is a friend of the current user
+     *
+     * @param user user to check for friend status
+     * @return boolean indicating mutual friendship status
+     */
+    public boolean isFriendOf(User user) {
+        return this.friends.contains(user) && user.friends.contains(this);
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setPhone(){
-        this.phone = phone;
-    }
-
-    public void setLogin() { this.loginStatus = true; }
-
-    public void setLogout() {this.loginStatus = false; }
-
-    public void setLastActiveTime(Timestamp lastActiveTime){
-        this.lastActiveTime = lastActiveTime;
-    }
-
-    public void setBlackListEmpty(){
-        this.blackList = new ArrayList<User>();
-    }
-
-    public void setStudentProfile(){
-        // overload this method with different arguments to set different options in a profile.
-    }
-
-//some methods that would help friendRequest backend:
-    public static boolean isFriend(User user1, User user2){
-        return user1.friends.contains(user2) & user2.friends.contains(user1);
-    } //mutually
-
-    public void addFriend(User user){
+    /**
+     * Adds a user to friend list and removes them from blacklist.
+     *
+     * @param user user to add to friendlist
+     */
+    public void addFriend(User user) {
         this.blackList.remove(user);
         this.friends.add(user);
         user.friends.add(this);
-    } //mutually, add as a friend automatically means not ghosting this user anymore.
+    }
 
-    public void removeFriend(User user){
+    /**
+     * Mutually excludes friendship
+     * (i.e. removes the target user from friend list and removes
+     * the current user from the target's friend list)
+     *
+     * @param user user to remove from friend list
+     */
+    public void removeFriend(User user) {
         this.friends.remove(user);
         user.friends.remove(this);
-    } //mutually, remove a friend from your friend list automatically remove you from his/her friend list.
+    }
 
-    public void ghostFriend(User user){
+    /**
+     * Removes the target user from the friend list and
+     * adds them to the black list.
+     *
+     * @param user
+     */
+    public void ghostFriend(User user) {
         this.removeFriend(user);
         this.blackList.add(user);
     }
 
-    public void unghostFriend(User user){
+    /**
+     * Removes the target user from the black list
+     *
+     * @param user
+     */
+    public void unghostFriend(User user) {
         this.blackList.remove(user);
     }
 
@@ -175,17 +142,23 @@ public class User {
         return this.name;
     }
 
+    /**
+     * Checks whether the target object is of instance user and
+     * has the same id as the current user.
+     *
+     * @param o target object to check for equivalence
+     * @return boolean indicating equivalence of target object and current user
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
         }
 
-        if (!(o instanceof User)) {
+        if (!(o instanceof User user)) {
             return false;
         }
 
-        User user = (User) o;
         return this.id.equals(user.id);
     }
 }
