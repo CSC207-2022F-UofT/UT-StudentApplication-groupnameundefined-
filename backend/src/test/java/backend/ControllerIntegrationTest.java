9a0f657127df3fe116e41c254c0168a60197c2af
Alan Lin <shijun.lin@leadvisor.com>
2022-12-01 22:24:56 -0500

    Updated tests

---
+++ backend/src/test/java/backend/ControllerIntegrationTest.java
@@ -0,0 +1,20 @@
+package backend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerIntegrationTest {
+
+	public static String toJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
--- backend/src/test/java/backend/UserControllerIntegrationTest.java
+++ backend/src/test/java/backend/UserControllerIntegrationTest.java
@@ -1,11 +1,13 @@
-package backend.user;
+package backend;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,13 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import backend.controller.UserController;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-public class UserControllerIntegrationTests {
+import backend.form.UserForm.*;
+
+
+public class UserControllerIntegrationTest extends ControllerIntegrationTest {
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -32,12 +37,41 @@ public class UserControllerIntegrationTests {
 	}
 
 	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/api/user/greeting"))
+	public void registerUserAPI_expectSuccess() throws Exception {
+		// Valid input
+		RegisterForm input = new RegisterForm(
+				"Test Name",
+				"test.name@email.com",
+				"1234567Ab",
+				"0123456789"
+		);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/user/register")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(toJsonString(input)))
 				.andExpect(status().isOk())
-				.andExpect(content()
-						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-				.andExpect(content().json().match();));
+				.andExpect(jsonPath("$.id", is(1)))
+				.andExpect(jsonPath("$.email", is("test.name@email.com")))
+				.andExpect(jsonPath("$.studentProfile", nullValue()))
+				.andExpect(jsonPath("$.sentFriendRequests", hasSize(0)));
+	}
+
+	@Test
+	public void registerUserAPI_expectInvalidInput() throws Exception {
+		// Input with invalid email, password not meeting requirement, phone too short
+		RegisterForm input = new RegisterForm(
+				"Test Name",
+				"test.nameemail.com",
+				"1234567A",
+				"1234567"
+		);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/user/register")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(toJsonString(input)))
+				.andExpect(status().isBadRequest())
+				.andExpect(jsonPath("$.errors", hasSize(3)))
+				.andExpect(jsonPath("$.errors[*].field", containsInAnyOrder("email", "password", "phone")));
 	}
 
 }
